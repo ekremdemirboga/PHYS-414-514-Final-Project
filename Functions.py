87916481e91b099f6521cfa8a37e5243675b97f4
ekremdemirboga <ekremdemirboga@gmail.com>
2019-12-11 19:37:30 +0300

    added some functions

--- Functions.py
+++ Functions.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[21]:
+# In[2]:
 
 
 import csv
 import numpy as np
 
 def data_reader(file_name='white_dwarf_data.csv'):
-    ### Reads the file and returns columns as arrays ###
-    star_id = []
+    ### Reads the file and returns columns as sorted arrays ###
     star_masses = []
     star_surface_g = []
     line_count = 0
@@ -19,18 +18,33 @@ def data_reader(file_name='white_dwarf_data.csv'):
             if line_count == 0:
                 line_count +=1
             else:
-                star_id.append(row[0])
                 star_surface_g.append(float(row[1]))
                 star_masses.append(float(row[2]))
                 line_count += 1
                 
-    star_id = np.array(star_id)
-    star_masses = np.array(star_masses)
-    star_surface_g = np.array(star_surface_g)
+    star_masses = np.sort(np.array(star_masses))
+    star_surface_g = np.sort(np.array(star_surface_g))
+    
 
     return star_id,star_masses,star_surface_g
             
-            
+def plot_m_vs_R(M,R):
+    #constants
+    solar_mass = 1.988e30 #kg
+    G = 6.67408e-11
+    earth_radius = 6.371e6 #m
+    
+    star,mass,logg = data_reader()
+    g = (10**logg)*(10**(-2)) # in SI units
+    mass = mass * solar_mass # kg
+    R = np.sqrt(G*mass/g) #m
+    
+    plt.xlabel("Mass of the stars(solar_mass)")
+    plt.ylabel("Radius of the stars(earth radii)")
+    plt.title("Mass vs Radius for WDs")
+    
+    figure = plt.plot(mass/solar_mass,R/earth_radius,'o')
+    return figure
 
 
 # In[ ]:
